155347a092597f5759de5abe2cb98bad91e476c9
lyj2174 <lyj2174@gmail.com>
2025-09-23 15:49:59 +0900

    update homepage text for 0.2

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Hello MSA!
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -42,7 +42,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+            Hello Elephant!
           </a>
         </div>
       </main>
